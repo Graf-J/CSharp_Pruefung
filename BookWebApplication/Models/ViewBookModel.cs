0627730135287e2d8e426b601ae31907756b4d8e
Graf-J <johannes.graf.1908@gmail.com>
2023-07-15 14:01:39 +0200

    Add ViewBookModel

---
+++ BookWebApplication/Models/ViewBookModel.cs
@@ -0,0 +1,10 @@
+﻿using BookRepository.Models;
+
+namespace BookWebApplication.Models
+{
+    public class ViewBookModel
+    {
+        public List<Book> CurrentBooks { get; set; } = new();
+        public List<Book> ArchivedBooks { get; set; } = new();
+    }
+}
