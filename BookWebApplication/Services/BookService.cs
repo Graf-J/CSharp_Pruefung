06fe2b31edb8a28bd1cad4a97d84e57d6c5816cb
Graf-J <johannes.graf.1908@gmail.com>
2023-07-15 16:58:31 +0200

    Improve User Interface

--- BookWebApplication/Services/BookService.cs
+++ BookWebApplication/Services/BookService.cs
@@ -4,15 +4,26 @@ using Microsoft.EntityFrameworkCore;
 
 namespace BookWebApplication.Services
 {
+    /// <summary>
+    /// Service class for handling book-related operations.
+    /// </summary>
     public class BookService : IBookService
     {
         private readonly DataContext _context;
 
+        /// <summary>
+        /// Initializes a new instance of the BookService class with the provided DataContext.
+        /// </summary>
+        /// <param name="context">The DataContext to be used for database operations.</param>
         public BookService(DataContext context)
         {
             _context= context;
         }
 
+        /// <summary>
+        /// Retrieves a list of current books asynchronously from the database.
+        /// </summary>
+        /// <returns>A Task that represents the asynchronous operation. The task result contains the list of current books.</returns>
         public async Task<List<CurrentBook>> GetCurrentBooksAsync()
         {
             var currentBooks = await _context.CurrentBooks.ToListAsync();
@@ -20,7 +31,10 @@ namespace BookWebApplication.Services
             return currentBooks;
         }
 
-
+        /// <summary>
+        /// Retrieves a list of archived books asynchronously from the database.
+        /// </summary>
+        /// <returns>A Task that represents the asynchronous operation. The task result contains the list of archived books.</returns>
         public async Task<List<ArchivedBook>> GetArchivedBooksAsync()
         {
             var archivedBooks = await _context.ArchivedBooks.ToListAsync();
@@ -28,6 +42,11 @@ namespace BookWebApplication.Services
             return archivedBooks;
         }
 
+        /// <summary>
+        /// Moves a book from archived books to current books asynchronously.
+        /// </summary>
+        /// <param name="bookId">The ID of the book to be moved.</param>
+        /// <returns>A Task that represents the asynchronous operation.</returns>
         public async Task MoveBookToCurrentAsync(int bookId)
         {
             // Find the Book which has to be moved by its ID
@@ -50,6 +69,11 @@ namespace BookWebApplication.Services
             await _context.SaveChangesAsync();
         }
 
+        /// <summary>
+        /// Moves a book from current books to archived books asynchronously.
+        /// </summary>
+        /// <param name="bookId">The ID of the book to be moved.</param>
+        /// <returns>A Task that represents the asynchronous operation.</returns>
         public async Task MoveBookToArchiveAsync(int bookId)
         {
             // Find the Book which has to be moved by its ID
